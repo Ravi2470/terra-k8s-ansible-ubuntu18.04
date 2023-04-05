a5487d33dbd198b7db578727fbba144cb0910237
Ravi2470 <109647098+Ravi2470@users.noreply.github.com>
2023-04-05 13:10:04 +0530

    Update providers.tf

--- terraform-ws/providers.tf
+++ terraform-ws/providers.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "AKIA4PQ5PXF3PPPFTGUP"
-  secret_key = "GTIEnAho3TbR204JVZhGoFMHBWkVtA8V20iZOBek"
+  access_key = "your-access-key"
+  secret_key = "your-secret-key"
 }
